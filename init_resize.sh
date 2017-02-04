663dbff59300d0a678188ac612fdfca3dca92452
Martin Wimpress <martin.wimpress@ubuntu.com>
2017-02-04 22:15:12 +0000

    Remove NOOBS checks. Ubuntu MATE compatible. Minor fixes.

--- init_resize.sh
+++ init_resize.sh
@@ -10,7 +10,7 @@ reboot_pi () {
 
 check_commands () {
   if ! command -v whiptail > /dev/null; then
-      echo ="whiptail not found"
+      FAIL_REASON="whiptail not found"
       sleep 5
       return 1
   fi
@@ -21,14 +21,6 @@ check_commands () {
     fi
   done
   return 0
-}
-
-check_noobs () {
-  if [ "$BOOT_PART_DEV" = "/dev/mmcblk0p1" ]; then
-    NOOBS=0
-  else
-    NOOBS=1
-  fi
 }
 
 get_variables () {
@@ -43,8 +35,6 @@ get_variables () {
   BOOT_DEV_NAME=`echo /sys/block/*/${BOOT_PART_NAME} | cut -d "/" -f 4`
   BOOT_PART_NUM=`cat /sys/block/${BOOT_DEV_NAME}/${BOOT_PART_NAME}/partition`
 
-  check_noobs
-
   ROOT_DEV_SIZE=`cat /sys/block/${ROOT_DEV_NAME}/size`
   TARGET_END=`expr $ROOT_DEV_SIZE - 1`
 
@@ -55,25 +45,9 @@ get_variables () {
   ROOT_PART_LINE=`echo "$PARTITION_TABLE" | grep -e "^${ROOT_PART_NUM}:"`
   ROOT_PART_START=`echo $ROOT_PART_LINE | cut -d ":" -f 2`
   ROOT_PART_END=`echo $ROOT_PART_LINE | cut -d ":" -f 3`
-
-  if [ "$NOOBS" = "1" ]; then
-    EXT_PART_LINE=`echo "$PARTITION_TABLE" | grep ":::;" | head -n 1`
-    EXT_PART_NUM=`echo $EXT_PART_LINE | cut -d ":" -f 1`
-    EXT_PART_START=`echo $EXT_PART_LINE | cut -d ":" -f 2`
-    EXT_PART_END=`echo $EXT_PART_LINE | cut -d ":" -f 3`
-  fi
 }
 
 check_variables () {
-  if [ "$NOOBS" = "1" ]; then
-    if [ $EXT_PART_NUM -gt 4 ] || \
-       [ $EXT_PART_START -gt $ROOT_PART_START ] || \
-       [ $EXT_PART_END -lt $ROOT_PART_END ]; then
-      FAIL_REASON="Unsupported extended partition"
-      return 1
-    fi
-  fi
-
   if [ $ROOT_PART_NUM -ne $LAST_PART_NUM ]; then
     FAIL_REASON="Root partition should be last partition"
     return 1
@@ -97,24 +71,8 @@ main () {
     return 1
   fi
 
-  if [ "$NOOBS" = "1" ]; then
-    BCM_MODULE=`cat /proc/cpuinfo | grep -e "^Hardware" | cut -d ":" -f 2 | tr -d " " | tr '[:upper:]' '[:lower:]'`
-    if ! modprobe $BCM_MODULE; then
-      FAIL_REASON="Couldn't load BCM module $BCM_MODULE"
-      return 1
-    fi
-    echo $BOOT_PART_NUM > /sys/module/${BCM_MODULE}/parameters/reboot_part
-  fi
-
   if [ $ROOT_PART_END -eq $TARGET_END ]; then
     reboot_pi
-  fi
-
-  if [ "$NOOBS" = "1" ]; then
-    if ! parted -m $ROOT_DEV u s resizepart $EXT_PART_NUM yes $TARGET_END; then
-      FAIL_REASON="Extended partition resize failed"
-      return 1
-    fi
   fi
 
 if ! parted -m $ROOT_DEV u s resizepart $ROOT_PART_NUM $TARGET_END; then
@@ -129,7 +87,7 @@ mount -t proc proc /proc
 mount -t sysfs sys /sys
 
 mount /boot
-sed -i 's| quiet init=/usr/lib/raspi-config/init_resize.sh||' /boot/cmdline.txt
+sed -i 's| init=/usr/lib/raspi-config/init_resize.sh||' /boot/cmdline.txt
 mount /boot -o remount,ro
 sync
 
@@ -140,11 +98,11 @@ if ! check_commands; then
 fi
 
 if main; then
-  whiptail --infobox "Resized root filesystem. Rebooting in 5 seconds..." 20 60
+  whiptail --infobox "Resized root partition. Rebooting in 5 seconds..." 20 60
   sleep 5
 else
   sleep 5
-  whiptail --msgbox "Could not expand filesystem, please try raspi-config or rc_gui.\n${FAIL_REASON}" 20 60
+  whiptail --msgbox "Could not expand filesystem, please try raspi-config.\n${FAIL_REASON}" 20 60
 fi
 
 reboot_pi
